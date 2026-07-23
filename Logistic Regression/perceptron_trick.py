807bac364cae2fe70d9b88ed60a01b783f4bfc95
varunXsensei <varun.saxena.dev27@gmail.com>
2026-07-23 14:19:10 +0530

    Add files via upload

---
+++ Logistic Regression/perceptron_trick.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def step(z):
+    if z > 0: return 1
+    return 0
+
+class Perceptron:
+    def __init__(self,n_iter = 1000,lr = 0.1):
+        self.weights = None
+        self.lr = lr
+        self.n_iter = n_iter
+    
+    def fit(self,X,y):
+        X = np.insert(X,0,1,axis = 1)
+        self.weights = np.ones(X.shape[1])
+        for i in range(self.n_iter):
+            j = np.random.randint(0,100)
+            y_pred = step(np.dot(X[j],self.weights))
+            self.weights = self.weights + self.lr*(y[j]-y_pred)*X[j]    
+        return self.weights[0],self.weights[1:]    
---
+++ Logistic Regression/perceptron_trick_test.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import make_classification
+import numpy as np
+import matplotlib.pyplot as plt
+from perceptron_trick import Perceptron
+
+X,y = make_classification(n_samples=100,n_features=2,n_informative=1,n_redundant=0,n_classes=2,n_clusters_per_class=1,random_state=41,hypercube=False,class_sep=20)
+
+
+
+model = Perceptron()
+intercept,coef = model.fit(X,y)
+
+m = -(coef[0]/coef[1])
+b = -(intercept/coef[1])
+
+x_input = np.linspace(-3,3,100)
+y_input = m*x_input + b
+
+
+plt.plot(x_input,y_input,color ='red',linewidth = 3)
+plt.scatter(X[:,0],X[:,1],c = y)
+plt.ylim(-3,2)
+plt.show()
